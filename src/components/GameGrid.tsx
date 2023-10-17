2aedfda42f2530dc507bac28d8d21f56fd76747a
Joveth-Dev <jovethespiritu.developer@gmail.com>
2023-10-17 21:47:24 +0800

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,8 +13,8 @@ const GameGrid = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
         padding="10px"
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
